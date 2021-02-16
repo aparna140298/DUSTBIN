80a379082cc60c3995c2a912fa0f8c4519e2290d
aparna140298 <61573654+aparna140298@users.noreply.github.com>
2021-02-16 18:11:42 +0530

    Add files via upload

---
+++ pro-25-trial-main/sketch.js
@@ -0,0 +1,46 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+var ball2
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	ball2=new ball (200,450,70)
+    
+	
+    
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+  ball2.display();
+
+}
+function keyPressed() {
+if(keyDown(UP_ARROW)){
+	Matter.Body.applyForce(ball2.body,ball2.body.position,{x:130,y:-145})
+}
+}
